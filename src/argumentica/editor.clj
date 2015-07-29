4d4495fa9bb3bb077c4b5ccc8026f88135bd44aa
Jukka Villstedt <jukka.villstedt@gmail.com>
2015-07-30 00:14:58 +0300

    editing new conclusions work

--- src/argumentica/editor.clj
+++ src/argumentica/editor.clj
@@ -115,12 +115,56 @@
               :argumentica.argument/title title} ])))
 
 
-(let [conn (create-database)]
-  (d/transact conn
-              (add-argument (d/db conn) "argument 1" "conclusion" "premise 1" "premise 2"))
-  (println "foo:" (let [db (d/db conn)
-                        entity-id (first (main-conclusions db))]
-                    (value db entity-id :argumentica.sentence/text)))
+(defn set-attribute-value [transaction entity-id attribute value]
+  (println "setting" transaction entity-id attribute value)
+  (let [updated-transaction (reduce (fn [updated-transaction statement-map]
+                                      (if (= entity-id (:db/id statement-map))
+                                        (conj updated-transaction
+                                              (assoc statement-map
+                                                     attribute value))
+                                        (conj updated-transaction
+                                              statement-map)))
+                                    []
+                                    transaction)]
+    (if (= transaction updated-transaction)
+      (conj transaction
+            {:db/id entity-id
+             attribute value})
+      updated-transaction)))
+
+(defn tempids [transaction]
+  (->> transaction
+       (map :db/id)
+       (filter #(instance? datomic.db.DbId %))))
+
+(defn ids-to-tempids [db transaction-tempids tempids]
+  (reduce (fn [result tempid]
+            (assoc result
+                   (d/resolve-tempid db
+                                     transaction-tempids
+                                     tempid)
+                   tempid))
+          {}
+          tempids))
+
+(let [conn (create-database)
+      tempid (d/tempid :db.part/user)
+      transaction [{:db/id tempid
+                    :argumentica.argument/title "Foo"}]
+      result @(d/transact conn
+                          transaction)]
+  (println (tempids transaction))
+  (println (instance? datomic.db.DbId tempid))
+  (println (type tempid)  tempid (:tempids result))
+  (println (d/resolve-tempid (:db-after result)
+                             (:tempids result) tempid))
+  (println (ids-to-tempids (:db-after result)
+                           (:tempids result)
+                           (tempids transaction)))
+  
+  #_(println "foo:" (let [db (d/db conn)
+                          entity-id (first (main-conclusions db))]
+                      (value db entity-id :argumentica.sentence/text)))
   #_(println "foo:" (main-conclusions (d/db conn))
              #_(-> (d/entity (d/db conn)
                              (sentence-by-text (d/db conn) "conclusion"))
@@ -154,6 +198,16 @@
                           :argumentica.argument/main-conclusion
                           :argumentica.sentence/text))))
 
+(defn set-changes [state changes]
+  (let [result (d/with (:db state)
+                       changes)]
+    (assoc state
+           :changes changes
+           :db-with-changes (:db-after result)
+           :ids-to-tempids-map (ids-to-tempids (:db-after result)
+                                               (:tempids result)
+                                               (tempids changes)))))
+
 (defn attribute-editor [view-context db db-with-changes state entity-id attribute]
   (let [old-value (value db
                          entity-id
@@ -167,7 +221,14 @@
                         (update-in [:constructor-overrides] assoc [:on-change :text] (fn [state new-value]
                                                                                        (gui/apply-to-local-state state
                                                                                                                  view-context
-                                                                                                                 assoc-in [:changes changed-value-key] new-value))))
+                                                                                                                 (fn [state]
+                                                                                                                   (set-changes state
+                                                                                                                                (set-attribute-value (:changes state)
+                                                                                                                                                     (or (get (:ids-to-tempids-map state)
+                                                                                                                                                              entity-id)
+                                                                                                                                                         entity-id)
+                                                                                                                                                     :argumentica.sentence/text
+                                                                                                                                                     new-value)))))))
                     (when (not= old-value new-value)
                       (text "*" [255 0 0 255] 30)))))
 
@@ -178,52 +239,48 @@
                      (text text-value)]))
 
 
-(defn changes-to-transaction [changes]
-  (reduce (fn [transaction [[id attribute] value]]
-            (conj transaction {:db/id id
-                               attribute value}))
-          []
-          changes))
+
 
 (defn argumentica-root-view [view-context state]
-  (l/vertically (let [db-with-changes (:db-after (d/with (:db state)
-                                                         (changes-to-transaction (:changes state))))]
-                  (for [conclusion (main-conclusions db-with-changes)]
-                    (attribute-editor view-context
-                                      (:db state)
-                                      db-with-changes
-                                      state
-                                      conclusion
-                                      :argumentica.sentence/text)))
-                #_(-> (button "New")
-                    (gui/on-mouse-clicked-with-view-context view-context
-                                                            (fn [state event]
-                                                              (d/transact (:conn state)
-                                                                          (add-argument db-with-changes "New argument" "conclusion"))
-                                                              (assoc state :changes {}
-                                                                     :db (d/db (:conn state))))))
-                (-> (button "Save")
-                    (gui/on-mouse-clicked-with-view-context view-context
-                                                            (fn [state event]
-                                                              (d/transact (:conn state)
-                                                                          (changes-to-transaction (:changes state)))
-                                                              (assoc state :changes {}
-                                                                     :db (d/db (:conn state))))))
-                (-> (button "Refresh")
-                    (gui/on-mouse-clicked-with-view-context view-context
-                                                            (fn [state event]
-                                                              (assoc state :db (d/db (:conn state))))))
-                (-> (button "Cancel")
-                    (gui/on-mouse-clicked-with-view-context view-context
-                                                            (fn [state event]
-                                                              (assoc state :changes {}))))
-                (text (:changes state))))
+  (l/vertically 
+   (for [conclusion (main-conclusions (:db-with-changes state))]
+     (attribute-editor view-context
+                       (:db state)
+                       (:db-with-changes state)
+                       state
+                       conclusion
+                       :argumentica.sentence/text))
+   (-> (button "New")
+       (gui/on-mouse-clicked-with-view-context view-context
+                                               (fn [state event]
+                                                 (set-changes state (concat (:changes state)
+                                                                            (add-argument (:db-with-changes state) "New argument" "conclusion"))))))
+   (-> (button "Save")
+       (gui/on-mouse-clicked-with-view-context view-context
+                                               (fn [state event]
+                                                 (d/transact (:conn state)
+                                                             (:changes state))
+                                                 (-> state
+                                                     (assoc :db (d/db (:conn state)))
+                                                     (set-changes [])))))
+   (-> (button "Refresh")
+       (gui/on-mouse-clicked-with-view-context view-context
+                                               (fn [state event]
+                                                 (-> state
+                                                     (assoc :db (d/db (:conn state)))
+                                                     (set-changes (:changes state))))))
+   (-> (button "Cancel")
+       (gui/on-mouse-clicked-with-view-context view-context
+                                               (fn [state event]
+                                                 (set-changes state []))))
+   (text (:changes state))))
 
 (defn argumentica-root [conn]
   (fn [view-context]
-    {:local-state {:conn conn
-                   :changes {}
-                   :db (d/db conn)}
+    {:local-state (-> {:conn conn
+                       :db (d/db conn)}
+                      (set-changes []))
+     
      :view #'argumentica-root-view}))
 
 (def connection (let [connection (create-database)]
@@ -261,4 +318,7 @@
 
   #_(profiler/with-profiler (gui/start-control argumentica-root)))
 
+;; TODO
+;; changing new arguents conclusion does not work because d/with generates a new entity id every time
+;; use resolveTempId to build a translation table that is used to manipulate the transaction
 
