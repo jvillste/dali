04a1b7cf7d6e95158a3c3560aa1cefb86b0c50ac
Jukka Villstedt <jukka.villstedt@nitor.fi>
2017-07-16 00:16:24 +0300

    refine ibis layout

--- src/argumentica/ibis.clj
+++ src/argumentica/ibis.clj
@@ -28,36 +28,125 @@
     :question [255 255 255 255]
     :idea [255 255 0 255]
     :pro [0 255 0 255]
-    :con [255 0 0 255]
+    :con [255 100 100 255]
     [0 255 255 255]))
 
-(defn node-view [node]
-  (layouts/vertically
-   (visuals/text (:text node) (node-color (:type node)))
-   (when (:children node)
-     (layouts/horizontally (assoc (visuals/rectangle [255 255 255 255]
-                                                     0 0)
-                                  :width 1
-                                  :height 10)
-                           (apply layouts/vertically (for [child (:children node)]
+(defn node-icon [node-type]
+  (layouts/with-margins 5 5 0 0
+    (visuals/rectangle (node-color node-type)
+                       10 10
+                       15 15)))
+
+#_(defn node-view [node]
+    (let [child-layout (-> (apply layouts/vertically (for [[index child] (map-indexed vector (:children node))]
                                                        (layouts/horizontally (layouts/with-margins 10 0 0 0
                                                                                (assoc (visuals/rectangle [255 255 255 255]
                                                                                                          0 0)
-                                                                                      :width 10
+                                                                                      :width 7
                                                                                       :height 1))
-                                                                             (node-view child))))))))
+                                                                             
+                                                                             (node-view child))))
+                           (application/do-layout java.lang.Integer/MAX_VALUE
+                                                  java.lang.Integer/MAX_VALUE))]
+      (layouts/vertically
+       (layouts/horizontally
+        (layouts/vertically (node-icon (:type node))
+                            
+                            )
+        (layouts/with-maximum-size 500 nil (visuals/text-area (:text node) [255 255 255 255])))
+       (when (:children node)
+         (layouts/with-margins 0 0 0 7 (layouts/horizontally (assoc (visuals/rectangle [255 255 255 255]
+                                                                                       0 0)
+                                                                    :width 1
+                                                                    :height (+ 10
+                                                                               (- (:height child-layout)
+                                                                                  (:height (last (:children child-layout))))))
+                                                             
+                                                             child-layout))))))
+
+#_(defn node-view [node]
+    (let [child-layout (-> (apply layouts/vertically (for [[index child] (map-indexed vector (:children node))]
+                                                       (layouts/horizontally (layouts/with-margins 10 0 0 0
+                                                                               (assoc (visuals/rectangle [255 255 255 255]
+                                                                                                         0 0)
+                                                                                      :width 7
+                                                                                      :height 1))
+                                                                             
+                                                                             (node-view child))))
+                           (application/do-layout java.lang.Integer/MAX_VALUE
+                                                  java.lang.Integer/MAX_VALUE))
+          text-layout (-> (layouts/with-maximum-size 500 nil (visuals/text-area (:text node) [255 255 255 255]))
+                          (application/do-layout java.lang.Integer/MAX_VALUE
+                                                 java.lang.Integer/MAX_VALUE))]
+      (layouts/horizontally
+       (layouts/vertically (node-icon (:type node))
+                           (when (:children node)
+                             (layouts/with-margins 0 0 0 7
+                               (assoc (visuals/rectangle [255 255 255 255]
+                                                         0 0)
+                                      :width 1
+                                      :height (- (+ (:height text-layout)
+                                                    (- (:height child-layout)
+                                                       (:height (last (:children child-layout)))))
+                                                 10)))))
+       (layouts/vertically text-layout
+                           child-layout))))
+
+(defn node-view [node]
+  (let [child-layout (-> (apply layouts/vertically (for [[index child] (map-indexed vector (:children node))]
+                                                     (layouts/horizontally (layouts/with-margins 12 0 0 0
+                                                                             (assoc (visuals/rectangle [255 255 255 255]
+                                                                                                       0 0)
+                                                                                    :width 7
+                                                                                    :height 1))
+                                                                           
+                                                                           (node-view child))))
+                         (application/do-layout java.lang.Integer/MAX_VALUE
+                                                java.lang.Integer/MAX_VALUE))
+        text-layout (-> (layouts/with-margins 3 0 5 0
+                          (layouts/with-maximum-size 500 nil (visuals/text-area (:text node) [255 255 255 255])))
+                        (application/do-layout java.lang.Integer/MAX_VALUE
+                                               java.lang.Integer/MAX_VALUE))]
+    (layouts/vertically
+     (layouts/horizontally
+      (layouts/vertically (node-icon (:type node))
+                          (when (:children node)
+                            (layouts/with-margins 0 0 0 7
+                              (assoc (visuals/rectangle [255 255 255 255]
+                                                        0 0)
+                                     :width 1
+                                     :height (- (:height text-layout)
+                                                20)))))
+      text-layout)
+     (when (:children node)
+       (layouts/horizontally
+        (layouts/with-margins 0 0 0 7
+          (assoc (visuals/rectangle [255 255 255 255]
+                                    0 0)
+                 :width 1
+                 :height (+ 12
+                            (- (:height child-layout)
+                               (:height (last (:children child-layout)))))))
+        child-layout)))))
 
 
-(defn create-scene-graph [width height]
+(defn create-scene-graph [node-atom width height]
   (animation/swap-state! animation/set-wake-up 1000)
-  (-> (node-view (q "question 1"
-                    (i "idea 1"
-                       (p "pro 1")
-                       (c "con 1"
-                          (p "pro 2")))))
+  (-> (layouts/with-margins 10 10 10 10
+        (node-view @node-atom))
       (application/do-layout width height)))
 
+(defn visualize [node-atom]
+  (application/start-window (partial create-scene-graph
+                                     node-atom)))
+
 (defn start []
-  (application/start-window #'create-scene-graph)
+  (application/start-window (partial create-scene-graph
+                                     (atom (q "question 1 sf sfs dfsd fsd fds fdsf dsf sd fsdf sdf ss fssd f"
+                                              (i "idea 1 1 sf sfs dfsd fsd fds fdsf dsf sd fsdf sdf ss fssd f"
+                                                 (p "pro 1")
+                                                 (c "con 1"
+                                                    (p "pro 2"))
+                                                 (c "con 2"))))))
   #_(.start (Thread. (fn []
                        (start-window)))))
