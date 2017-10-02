43b0550fa033af6fc69ae17939b86be58880d83a
Jukka Villstedt <jukka.villstedt@nitor.fi>
2017-10-02 08:39:32 +0300

    rename db_serever.clj to db_server.clj

--- src/argumentica/db_client.clj
+++ src/argumentica/db_client.clj
@@ -7,22 +7,30 @@
             [argumentica.command :as command]
             [argumentica.db :as db]))
 
+(defn handle-message [state-atom channel-handler-context message-string]
+  (log/with-exception-logging
+    (let [[call-id result] (read-string message-string)]
+      (if-let [result-channel (get-in @state-atom [:result-channels call-id])]
+        (do (when result
+           (async/>!! result-channel
+                      result))
+            (async/close! result-channel))
+        
+        (do (log/write "client got message" (log/cut-string-to-max-length message-string
+                                                                          300))
+            (command/dispatch-command 'argumentica.db-client
+                                      (read-string message-string) 
+                                      channel-handler-context
+                                      state-atom))))))
+
 (def state-atom (let [state-atom (atom {:db (db/create)})]
                   (client/start-client 1337
                                        (reify
                                          pipeline/HandlerAdapter
                                          (channel-read [this channel-handler-context message-string]
-                                           (log/with-exception-logging
-                                             (let [[call-id result] (read-string message-string)]
-                                               (if-let [result-channel (get-in @state-atom [:result-channels call-id])]
-                                                 (async/>!! result-channel
-                                                            result)
-                                                 (do (log/write "client got message" (log/cut-string-to-max-length message-string
-                                                                                                                   300))
-                                                     (command/dispatch-command 'argumentica.db-client
-                                                                               (read-string message-string) 
-                                                                               channel-handler-context
-                                                                               state-atom))))))
+                                           (handle-message state-atom
+                                                           channel-handler-context
+                                                           message-string))
 
                                          pipeline/ChannelActive
                                          (channel-active [this channel-handler-context]
@@ -33,6 +41,13 @@
                                          (channel-inactive [this channel-handler-context]
                                            (log/write "client disconnected"))))
                   state-atom))
+
+(defn ^:public transact [channel-handler-context state-atom transaction]
+  (log/write "transacting client")
+  (swap! state-atom
+         update-in [:db]
+         db/transact
+         transaction))
 
 (defn call [state-atom command]
   (let [call-id (rand-int 1000)
@@ -48,7 +63,20 @@
                               (pr-str [call-id command]))
     result-channel))
 
+(defn call-and-log-result [message]
+  (async/go (let [result (async/<! (call state-atom message))]
+              (log/write "got result: " result))))
+
 (comment
-  (async/go (let [result (async/<! (call state-atom [:get-transaction-log]))]
-              (log/write "got result: " result)))
+
+  (call-and-log-result [:transact-over [:master] [[1] :friend :add "friend 2"]])
+
+  (call-and-log-result [:get-transaction-log])
+  
+  (call-and-log-result [:quit])
+
+
+
+  (get-in @state-atom
+          [:db :transaction-log])
   (stop))
--- src/argumentica/db_server.clj
+++ src/argumentica/db_server.clj
@@ -6,30 +6,33 @@
             [net.ty.pipeline :as pipeline]
             [net.ty.channel  :as channel]))
 
-
-
 (defn broadcast-other-channels [source-channel channel-group message]
   (doseq [destination-channel channel-group :when (not= destination-channel source-channel)]
     (channel/write-and-flush! destination-channel
                               message)))
 
-(defn ^:public transact-over [channel-handler-context state-atom parent-references & statements]
-  (swap! state-atom
-         update
-         :db
-         (fn [db]
-           (let [transaction (db/transaction-over db
-                                                  parent-references
-                                                  statements)
-                 db (db/transact-over db
-                                      (first parent-references)
-                                      transaction)]
 
-             (broadcast-other-channels (channel/channel channel-handler-context)
-                                       (:channel-group @state-atom)
-                                       (pr-str ['transacted transaction]))
-             
-             db)))
+(defn ^:public transact-over [channel-handler-context state-atom parent-references & statements]
+  (log/write "transact-over called")
+  (let [transaction-to-be-broadcasted (atom nil)
+        new-state (swap! state-atom
+                         update
+                         :db
+                         (fn [db]
+                           (let [transaction (db/transaction-over db
+                                                                  parent-references
+                                                                  statements)]
+                             (reset! transaction-to-be-broadcasted
+                                     transaction)
+
+                             (db/transact-over db
+                                               (first parent-references)
+                                               transaction))))]
+    (log/write "broadcasting " @transaction-to-be-broadcasted)
+    (broadcast-other-channels (channel/channel channel-handler-context)
+                              (:channel-group @state-atom)
+                              (pr-str ['transact @transaction-to-be-broadcasted])))
+  
   nil)
 
 (defn ^:public get-transaction-log [channel-handler-context state-atom]
@@ -38,19 +41,18 @@
       []))
 
 (defn ^:public quit [channel-handler-context state-atom]
-  (channel/close! channel-handler-context))
-
-
+  (channel/close! channel-handler-context)
+  nil)
 
 (defn handle-message [channel-handler-context state-atom message-string]
   (log/with-exception-logging
-    (log/write "server got message" (log/cut-string-to-max-length message-string
-                                                                  300))
+    (log/write "server got message: " (log/cut-string-to-max-length message-string
+                                                                    300))
     (let [[call-id message] (read-string message-string)]
       (when-let [result (command/dispatch-command 'argumentica.db-server
-                                                 message
-                                                 channel-handler-context
-                                                 state-atom)]
+                                                  message
+                                                  channel-handler-context
+                                                  state-atom)]
         
         (log/write "serever sends result: " (log/cut-string-to-max-length (pr-str result)
                                                                           300))
@@ -79,7 +81,6 @@
                                      (log/write "server got new connection")
                                      (channel/add-to-group (:channel-group @state-atom)
                                                            (channel/channel channel-handler-context)))))))
-
 
 (comment
   (stop))
