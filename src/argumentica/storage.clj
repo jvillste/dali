d4ce986943b281ca0a3cbf46548ac10a5bae6f6b
Jukka Villstedt <jukka.villstedt@nitor.fi>
2017-10-17 15:30:37 +0300

    add storage/next-seq-from

--- src/argumentica/storage.clj
+++ src/argumentica/storage.clj
@@ -52,8 +52,8 @@
         (assoc :root-id new-root-id)
         (assoc-in [:nodes old-root-id :values] lesser-sorted-set)
         (assoc-in [:nodes new-root-id] {:values (sorted-set median-value)
-                                        :children [old-root-id
-                                                   new-child-id]})
+                                        :child-ids [old-root-id
+                                                    new-child-id]})
         (assoc-in [:nodes new-child-id] {:values greater-sorted-set}))))
 
 
@@ -61,7 +61,7 @@
 (deftest test-split-root
   (is (= {:nodes
           {0 {:values #{1 2}},
-           1 {:values #{3}, :children [0 2]},
+           1 {:values #{3}, :child-ids [0 2]},
            2 {:values #{4 5}}},
           :next-node-id 3,
           :root-id 1}
@@ -95,18 +95,18 @@
         (update :next-node-id inc)
         (assoc-in [:nodes old-child-id :values] lesser-sorted-set)
         (update-in [:nodes parent-id :values] conj median-value)
-        (update-in [:nodes parent-id :children] insert-after old-child-id new-child-id)
+        (update-in [:nodes parent-id :child-ids] insert-after old-child-id new-child-id)
         (assoc-in [:nodes new-child-id] {:values greater-sorted-set}))))
 
 (deftest test-split-child
   (is (= {:nodes {0 {:values #{1}},
-                  1 {:values #{2 3}, :children '(0 3 2)},
+                  1 {:values #{2 3}, :child-ids '(0 3 2)},
                   2 {:values #{4 5}},
                   3 {:values #{3}}},
           :next-node-id 4,
           :root-id 1}
          (split-child {:nodes {0 {:values (sorted-set 1 2 3)},
-                               1 {:values (sorted-set 3), :children [0 2]},
+                               1 {:values (sorted-set 3), :child-ids [0 2]},
                                2 {:values (sorted-set 4 5)}},
                        :next-node-id 3,
                        :root-id 1}
@@ -138,17 +138,32 @@
          (child-index [2 4] 2))))
 
 (defn child-id [parent-node value]
-  (nth (:children parent-node)
-       (child-index (:values parent-node)
-                    value)))
+  (if-let [the-child-index (child-index (:values parent-node)
+                                        value)]
+    (nth (:child-ids parent-node)
+         the-child-index)
+    nil))
 
 (deftest test-child-id
   (is (= 3
-         (child-id {:values #{-1 3}, :children '(0 3 2)}
+         (child-id {:values #{-1 3}, :child-ids '(0 3 2)}
                    2))))
 
+(defn find-index [sequence value]
+  (first (keep-indexed (fn [index sequence-value]
+                         (if (= value sequence-value)
+                           index
+                           nil))
+                       sequence)))
+
+(deftest test-find-index
+  (is (= 2
+         (find-index [1 2 3] 3)))
+  (is (= nil
+         (find-index [1 2 3] 4))))
+
 (defn leaf-node? [node]
-  (not (:children node)))
+  (not (:child-ids node)))
 
 (defn node [storage node-id]
   (get-in storage [:nodes node-id]))
@@ -160,7 +175,6 @@
                    storage)
          previous-node-id nil
          node-id (:root-id storage)]
-    (prn "add loop" previous-node-id node-id)
     (let [the-node (node storage
                          node-id)]
       (if (leaf-node? the-node)
@@ -169,21 +183,19 @@
                    conj value)
         (if-let [the-child-id (child-id the-node
                                         value)]
-          (do (println "child is" the-child-id)
-              (let [storage (if ((:full? storage) (node storage
-                                                        the-child-id))
-                              (split-child storage
-                                           node-id
-                                           the-child-id)
-                              storage)]
-                (if-let [the-child-id (child-id (node storage
-                                                      node-id)
-                                                value)]
-                  (recur storage
-                         node-id
-                         the-child-id)
-                  (do (println "did not find child for " storage node-id value)
-                      storage))))
+          (let [storage (if ((:full? storage) (node storage
+                                                    the-child-id))
+                          (split-child storage
+                                       node-id
+                                       the-child-id)
+                          storage)]
+            (if-let [the-child-id (child-id (node storage
+                                                  node-id)
+                                            value)]
+              (recur storage
+                     node-id
+                     the-child-id)
+              storage))
           storage)))))
 
 
@@ -191,53 +203,120 @@
   (let [full? (fn [node]
                 (= 5 (count (:values node))))]
     (testing "root is full"
-        (is (= {:nodes
-                {0 {:values #{1 2}},
-                 1 {:values #{3}, :children [0 2]},
-                 2 {:values #{4 5 6}}},
-                :next-node-id 3,
-                :root-id 1,
-                :full? full?}
-               (add {:nodes {0 {:values (sorted-set 1 2 3 4 5)}}
-                     :next-node-id 1
-                     :root-id 0
-                     :full? full?}
-                    6))))
+      (is (= {:nodes
+              {0 {:values #{1 2}},
+               1 {:values #{3}, :child-ids [0 2]},
+               2 {:values #{4 5 6}}},
+              :next-node-id 3,
+              :root-id 1,
+              :full? full?}
+             (add {:nodes {0 {:values (sorted-set 1 2 3 4 5)}}
+                   :next-node-id 1
+                   :root-id 0
+                   :full? full?}
+                  6))))
 
     (testing "no splits needed"
-        (is (= {:nodes
-                {0 {:values (sorted-set -1 1 2)},
-                 1 {:values (sorted-set 3), :children [0 2]},
-                 2 {:values (sorted-set 4 5 6)}},
-                :next-node-id 3,
-                :root-id 1,
-                :full? full?}
-               (add {:nodes
-                     {0 {:values (sorted-set 1 2)},
-                      1 {:values (sorted-set 3), :children [0 2]},
-                      2 {:values (sorted-set 4 5 6)}},
-                     :next-node-id 3,
-                     :root-id 1,
-                     :full? full?}
-                    -1))))
+      (is (= {:nodes
+              {0 {:values (sorted-set -1 1 2)},
+               1 {:values (sorted-set 3), :child-ids [0 2]},
+               2 {:values (sorted-set 4 5 6)}},
+              :next-node-id 3,
+              :root-id 1,
+              :full? full?}
+             (add {:nodes
+                   {0 {:values (sorted-set 1 2)},
+                    1 {:values (sorted-set 3), :child-ids [0 2]},
+                    2 {:values (sorted-set 4 5 6)}},
+                   :next-node-id 3,
+                   :root-id 1,
+                   :full? full?}
+                  -1))))
 
-    (trace/with-trace-logging
-      (trace/trace-var #'split-child)
-      (testing "leaf is full"
-        (is (= {:nodes
-                {0 {:values #{-3 -2}},
-                 1 {:values #{-1 3}, :children '(0 3 2)},
-                 2 {:values #{4 5 6}},
-                 3 {:values #{0 1 2}}},
-                :next-node-id 4,
-                :root-id 1,
-                :full? full?}
-               (add {:nodes
-                     {0 {:values (sorted-set -3 -2 -1 0 1)},
-                      1 {:values (sorted-set 3), :children [0 2]},
-                      2 {:values (sorted-set 4 5 6)}},
-                     :next-node-id 3,
-                     :root-id 1,
-                     :full? full?}
-                    2)))))))
+    (testing "leaf is full"
+      (is (= {:nodes
+              {0 {:values #{-3 -2}},
+               1 {:values #{-1 3}, :child-ids '(0 3 2)},
+               2 {:values #{4 5 6}},
+               3 {:values #{0 1 2}}},
+              :next-node-id 4,
+              :root-id 1,
+              :full? full?}
+             (add {:nodes
+                   {0 {:values (sorted-set -3 -2 -1 0 1)},
+                    1 {:values (sorted-set 3), :child-ids [0 2]},
+                    2 {:values (sorted-set 4 5 6)}},
+                   :next-node-id 3,
+                   :root-id 1,
+                   :full? full?}
+                  2))))))
+
+(defn next-seq-from [storage value]
+  (loop [path []
+         node-id (:root-id storage)]
+    (let [the-node (node storage
+                         node-id)]
+      (if (leaf-node? the-node)
+        (subseq (:values the-node)
+                >=
+                value)
+        (if-let [the-child-id (child-id the-node
+                                        value)]
+          (recur storage
+                 the-child-id)
+          (let [splitter-index (find-index (:values the-node)
+                                           value)]
+            (concat [value]
+                    (:values (node storage
+                                   (get-in the-node
+                                           [:child-ids (inc splitter-index)]))))))))))
+
+(deftest test-next-seq-from
+  (is (= '(-2 -1 0 1)
+         (next-seq-from {:nodes
+                         {0 {:values (sorted-set -3 -2 -1 0 1)},
+                          1 {:values (sorted-set 3), :child-ids [0 2]},
+                          2 {:values (sorted-set 4 5 6)}},
+                         :next-node-id 3,
+                         :root-id 1}
+                        -2)))
+
+  (is (= '(3 4 5 6)
+         (next-seq-from {:nodes
+                         {0 {:values (sorted-set -3 -2 -1 0 1)},
+                          1 {:values (sorted-set 3), :child-ids [0 2]},
+                          2 {:values (sorted-set 4 5 6)}},
+                         :next-node-id 3,
+                         :root-id 1}
+                        3)))
+
+  (is (= '(6)
+         (next-seq-from {:nodes
+                         {0 {:values (sorted-set -3 -2 -1 0 1)},
+                          1 {:values (sorted-set 3), :child-ids [0 2]},
+                          2 {:values (sorted-set 4 5 6)}},
+                         :next-node-id 3,
+                         :root-id 1}
+                        6))))
+
+#_(defn seq-from [storage value]
+    (lazy-seq (cons n (to-seq (inc n)))))
+
+(defn chunked-to-seq [n]
+  (println n)
+  (lazy-seq (chunk-cons (range n) (to-seq (inc n)))))
+
+(comment
+  (take 5 (to-seq 10))
+
+  (take 5 (chunked-to-seq 10)))
+
+
+(defn persist [storage store]
+  
+  )
+
+#_(trace/with-trace-logging
+    (trace/trace-var #'split-child)
+    )
 
