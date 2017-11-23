bd9ad5decaca71dbb1016d51092a86298a124466
Jukka Villstedt <jukka.villstedt@iki.fi>
2017-11-23 07:20:04 +0200

    remove commented code

--- src/argumentica/match.clj
+++ src/argumentica/match.clj
@@ -13,20 +13,7 @@
 
        (def ~name (~(symbol (str type-name ".")))))))
 
-
-
-
-#_(do
-    (deftype
-        Any-string
-        []
-        java.lang.Object
-        (equals [_ value] (string? value)))
-    (defmethod print-method Any-string [this w] (.write w "any-string"))
-    (def any-string (Any-string.)))
-
 (def-matcher any-string [_ value] (string? value))
-
 (def-matcher any-function [_ value] (fn? value))
 
 
