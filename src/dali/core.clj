5cf08334804c0204ed5741e99ea88ce4b9b9c476
Jukka Villstedt <jukka.villstedt@iki.fi>
2018-07-04 14:08:34 +0300

    refine dali.core

--- src/dali/core.clj
+++ src/dali/core.clj
@@ -11,10 +11,10 @@
   (make-transient! [this])
   (make-persistent! [this]))
 
-(defprotocol Index
+(defprotocol SortedDatomSet
   (add-datom! [this datom])
   (subseq-from [this first-datom])
-  (last-indexed-transaction-number [this]))
+  (last-transaction-number [this]))
 
 (defprotocol Btree
   (store-root! [this metadata]))
@@ -30,14 +30,6 @@
   (transact! [this statements])
   (dereference-database [this]))
 
-(s/defschema DatabaseReference
-  {:transaction-log (s/protocol TransactionLog)
-   :indexes {s/Keyword (s/protocol Index)}})
-
-(s/defschema DatabaseValue
-  {:indexes {s/Keyword (s/protocol Index)}
-   :last-transaction-number s/Int})
-
 (s/defschema Command
   (s/enum :add :retract :set))
 
@@ -47,30 +39,50 @@
    (s/one Command "command")
    (s/one s/Any "value")])
 
+(s/defschema TransactionStatement
+  [(s/one s/Any "entity")
+   (s/one s/Keyword "attribute")
+   (s/one s/Int "transaction")
+   (s/one Command "command")
+   (s/one s/Any "value")])
+
 (s/defschema Transaction
   [Statement])
 
 (def validate-transaction (s/validator Transaction))
 
 (s/defschema Datom
+  "A vector that contains a transaction number along with other values."
   (s/both [s/Any]
           (s/pred (fn [datom] (some integer? datom))
                   "Contains a transaction number")))
 
-(s/defschema StatementToDatom
-  (s/=> Datom
+(s/defschema GenerateDatoms
+  "A function from database reference and transaction statement to datoms."
+  (s/=> [Datom]
 
-        DatabaseValue
-        s/Int ;; transaction number
-        s/Any ;; entity
-        s/Keyword ;; attribute
-        Command ;; command
-        s/Any ;; value
-        ))
+        DatabaseReference
+        TransactionStatement))
 
 (s/defschema IndexDefinition
-  {s/Keyword StatementToDatom})
+  {:key s/Keyword
+   :generate-datoms GenerateDatoms})
+
+(s/defschema Index
+  (merge IndexDefinition
+         {:sortred-datom-set (s/protocol SortedDatomSet)}))
+
+(s/defschema IndexMap
+  {s/Keyword IndexDefinitionWithIndex})
 
 (s/defschema Branch
   {:base-database DatabaseValue
    :branch-database DatabaseValue})
+
+(s/defschema DatabaseReference
+  {:transaction-log (s/protocol TransactionLog)
+   :index-map IndexMap})
+
+(s/defschema DatabaseValue
+  {:index-map IndexMap
+   :last-transaction-number s/Int})
