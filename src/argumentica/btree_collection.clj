2db362cee21babc21a70a047686c6826032ac58d
Jukka Villstedt <jukka.villstedt@iki.fi>
2020-09-06 06:59:16 +0300

    use atom in btree collection

--- src/argumentica/btree_collection.clj
+++ src/argumentica/btree_collection.clj
@@ -4,49 +4,54 @@
             [argumentica.directory-storage :as directory-storage]
             [argumentica.hash-map-storage :as hash-map-storage]
             [argumentica.mutable-collection :as mutable-collection]
+            [argumentica.transducible-collection :as transducible-collection]
             [argumentica.util :as util]
             [schema.core :as schema])
   (:import argumentica.comparator.DatomComparator))
 
-(defn- swap-btree! [btree-collection function & arguments]
-  (apply util/locking-swap-volatile!
-         (:btree-volatile btree-collection)
-         function
-         arguments))
+(defn locking-apply-to-btree! [btree-collection function & arguments]
+  (locking (:btree-atom btree-collection)
+    (reset! (:btree-atom btree-collection)
+            (apply function
+                   @(:btree-atom btree-collection)
+                   arguments)))
+  btree-collection)
 
-(defrecord BtreeCollection [btree-volatile]
+(defrecord BtreeCollection [btree-atom]
   clojure.lang.Sorted
   (comparator [this]
     (DatomComparator.))
   (entryKey [this entry]
     entry)
   (seq [this ascending?]
-    (if ascending?
-      (btree/inclusive-subsequence (:btree-volatile this)
-                                   ::comparator/min)
-      (btree/inclusive-reverse-subsequence (:btree-volatile this)
-                                           ::comparator/max)))
+    (locking (:btree-atom this)
+      (if ascending?
+        (btree/inclusive-subsequence (:btree-atom this)
+                                     ::comparator/min)
+        (btree/inclusive-reverse-subsequence (:btree-atom this)
+                                             ::comparator/max))))
   (seqFrom [this value ascending?]
-    (if ascending?
-      (btree/inclusive-subsequence (:btree-volatile this)
-                                   value)
-      (btree/inclusive-reverse-subsequence (:btree-volatile this)
-                                           value)))
+    (locking (:btree-atom this)
+      (if ascending?
+        (btree/inclusive-subsequence (:btree-atom this)
+                                     value)
+        (btree/inclusive-reverse-subsequence (:btree-atom this)
+                                             value))))
   mutable-collection/MutableCollection
   (add! [this value]
-    (util/locking-swap-volatile! (:btree-volatile this)
-                                 btree/add
-                                 value)))
+    (locking-apply-to-btree! this
+                          btree/add
+                          value))
+  transducible-collection/TransducibleCollection
+  (transduce [this value options]
+    (locking (:btree-atom this)
+      (apply btree/transduce-btree
+             (:btree-atom this)
+             value
+             options))))
 
 (defn- create-for-btree [btree]
-  (->BtreeCollection (volatile! btree)))
-
-(defn store-root! [btree-collection last-transaction-number]
-  (util/locking-swap-volatile! (:btree-volatile btree-collection)
-                               (fn [btree]
-                                 (-> btree
-                                     (btree/store-root {:last-transaction-number last-transaction-number})
-                                     (assoc :last-indexed-transaction-number last-transaction-number)))))
+  (->BtreeCollection (atom btree)))
 
 (def create-options {(schema/optional-key :node-size) schema/Int})
 
@@ -55,9 +60,9 @@
 (util/defno create-on-disk [base-path options :- create-options]
   (let [options (merge default-create-options
                        options)]
-   (create-for-btree (btree/create-from-options :metadata-storage (directory-storage/create (str base-path "/metadata"))
-                                                :node-storage (directory-storage/create (str base-path "/nodes"))
-                                                :full? (btree/full-after-maximum-number-of-values (:node-size options))))))
+    (create-for-btree (btree/create-from-options :metadata-storage (directory-storage/create (str base-path "/metadata"))
+                                                 :node-storage (directory-storage/create (str base-path "/nodes"))
+                                                 :full? (btree/full-after-maximum-number-of-values (:node-size options))))))
 
 (util/defno create-in-memory [options :- create-options]
   (let [options (merge default-create-options
--- test/argumentica/btree_collection_test.clj
+++ test/argumentica/btree_collection_test.clj
@@ -1,7 +1,8 @@
-(ns argumentica.db.common-test
+(ns argumentica.btree-collection-test
   (:require [argumentica.btree-collection :as btree-collection]
             [argumentica.mutable-collection :as mutable-collection]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [argumentica.btree :as btree]))
 
 (defn create-collection [& datoms]
   (let [collection (btree-collection/create-in-memory {:node-size 3})]
@@ -12,6 +13,14 @@
 (deftest test-btree-collection
   (is (= '(4 5 6)
          (subseq (create-collection 1 2 3 4 5 6)
+                 >=
+                 4))))
+
+(deftest test-unload
+  (is (= '(4 5 6)
+         (subseq (btree-collection/locking-apply-to-btree! (create-collection 1 2 3 4 5 6)
+                                                           btree/unload-excess-nodes
+                                                           0)
                  >=
                  4))))
 
