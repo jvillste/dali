3dab4cb3cfd68bb02c1e6fdf83a0dab56c8dc8e2
Jukka Villstedt <jukka.villstedt@iki.fi>
2022-01-16 07:54:08 +0200

    make SortedMapTransactionLog a type instead of a record

--- src/argumentica/sorted_map_transaction_log.clj
+++ src/argumentica/sorted_map_transaction_log.clj
@@ -3,20 +3,49 @@
             [argumentica.util :as util]
             [argumentica.reduction :as reduction]))
 
-(defrecord SortedMapTransactionLog [state-atom])
+
+(defprotocol SortedMapTransactionLogProtocol
+  (state-atom [this]))
+
+(deftype SortedMapTransactionLog [state-atom]
+  SortedMapTransactionLogProtocol
+  (state-atom [this]
+    state-atom)
+
+  clojure.lang.Seqable
+  (seq [this]
+    (map second (seq (:sorted-map @state-atom))))
+
+  clojure.lang.Sorted
+  (comparator [this]
+    (.comparator (:sorted-map @state-atom)))
+
+  (entryKey [this entry]
+    entry)
+
+  (seq [this ascending?]
+    (map second (.seq  (:sorted-map @state-atom)
+                       ascending?)))
+  (seqFrom [this value ascending?]
+    (map second (.seqFrom (:sorted-map @state-atom)
+                          value
+                          ascending?))))
+
+(defmethod print-method SortedMapTransactionLog [this ^java.io.Writer writer]
+  (.write writer (with-out-str (clojure.pprint/pprint (seq this)))))
 
 (defn create []
   (->SortedMapTransactionLog (atom {:sorted-map (sorted-map)})))
 
 (defmethod transaction-log/last-transaction-number SortedMapTransactionLog
   [this]
-  (:last-transaction-number @(:state-atom this)))
+  (:last-transaction-number @(state-atom this)))
 
 (defmethod transaction-log/add! SortedMapTransactionLog
   [this statements]
   (let [transaction-number (inc (or (transaction-log/last-transaction-number this)
                                     -1))]
-    (swap! (:state-atom this)
+    (swap! (state-atom this)
            (fn [state]
              (-> state
                  (update :sorted-map
@@ -31,7 +60,7 @@
   (reduction/reducible (fn [reducing-function initial-value]
                          (reduce reducing-function
                                  initial-value
-                                 (map second (subseq (:sorted-map @(:state-atom this))
+                                 (map second (subseq (:sorted-map @(state-atom this))
                                                      >=
                                                      first-transaction-number))))))
 
@@ -43,7 +72,7 @@
 
 (defmethod transaction-log/truncate! SortedMapTransactionLog
   [this first-preserved-transaction-number]
-  (swap! (:state-atom this)
+  (swap! (state-atom this)
          update
          :sorted-map
          util/filter-sorted-map-keys
