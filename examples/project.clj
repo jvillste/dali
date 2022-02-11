5a1caf72ce6dffde4f990223139ab1589f79cc36
Jukka Villstedt <jukka.villstedt@iki.fi>
2022-02-11 06:29:09 +0200

    move examples to attic

--- examples/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject examples "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [flow-gl/flow-gl "1.0.0-SNAPSHOT"]
-                 [dali "0.1.0-SNAPSHOT"]])
